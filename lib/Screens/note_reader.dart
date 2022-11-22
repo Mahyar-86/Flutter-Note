37d65e00e679d3f373555fb6c36acf843e64c754
Mahyar Kazazi <rihem1386@gmail.com>
2022-11-22 09:19:58 +0330

    first commit

---
+++ lib/Screens/note_reader.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:note/style/app_style.dart';
+
+class NoteReaderScreen extends StatefulWidget {
+  NoteReaderScreen(this.doc, {super.key});
+  QueryDocumentSnapshot doc;
+
+  @override
+  State<NoteReaderScreen> createState() => _NoteReaderScreenState();
+}
+
+class _NoteReaderScreenState extends State<NoteReaderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    int color_id = widget.doc["color_id"];
+    return Scaffold(
+      backgroundColor: AppStyle.cardsColor[color_id],
+      appBar: AppBar(
+        backgroundColor: AppStyle.cardsColor[color_id],
+        elevation: 0.0,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.doc["note_title"],
+              style: AppStyle.mainTitle,
+            ),
+            const SizedBox(
+              height: 4.0,
+            ),
+            Text(
+              widget.doc["creation_date"],
+              style: AppStyle.dateTitle,
+            ),
+            const SizedBox(
+              height: 18.0,
+            ),
+            Text(
+              widget.doc["note_content"],
+              style: AppStyle.mainContent,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
